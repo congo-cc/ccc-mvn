d9e7732c0dd5ba3f4882e7029f9d03de264b110b
Tamas Cservenak <tamas@cservenak.net>
2024-03-26 09:57:55 +0100

    Tidy up the plugin
    Changes:
    * compile against latest, set prerequisite
    * tied down all the plugins

--- org.congocc.maven.plugin/src/main/java/org/congocc/maven/plugin/GeneratorMojo.java
+++ org.congocc.maven.plugin/src/main/java/org/congocc/maven/plugin/GeneratorMojo.java
@@ -7,20 +7,20 @@ import java.util.Map;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
-import org.apache.maven.plugins.annotations.ResolutionScope;
 import org.apache.maven.project.MavenProject;
 
-@Mojo(name = "ccc-generate", defaultPhase = LifecyclePhase.GENERATE_SOURCES, requiresProject = true, requiresDependencyResolution = ResolutionScope.NONE)
+@Mojo(name = "ccc-generate", defaultPhase = LifecyclePhase.GENERATE_SOURCES)
 public class GeneratorMojo extends AbstractMojo {
 
 	/**
 	 * The current Maven project.
 	 *
 	 */
-	@Parameter(defaultValue = "${project}", readonly = true, required = true)
+	@Component
 	private MavenProject project;
 
 	@Parameter(property = "grammarFile", defaultValue = "${project.basedir}/src/main/ccc/Grammer.ccc")
@@ -40,18 +40,16 @@ public class GeneratorMojo extends AbstractMojo {
 
 	private Map<String, String> symbols = new HashMap<>();
 
+	@Override
 	public void execute() throws MojoExecutionException, MojoFailureException {
 		try {
-
 			int returnValue = org.congocc.app.Main.mainProgram(grammarFile.toPath(), outputDir.toPath(), lang, jdk,
 					quiet, symbols);
-
 			this.project.addCompileSourceRoot(outputDir.getAbsolutePath());
-			getLog().warn("return:" + returnValue);
-
+			getLog().info("return: " + returnValue);
 		} catch (Exception e) {
 			getLog().error(e);
-
+			throw new MojoExecutionException("CongoCC failure", e);
 		}
 	}
 }
